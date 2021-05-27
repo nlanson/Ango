4cb4d69a7f8170c54e68a78db182419d69e261be
Noah Lanson <lanson.noah@gmail.com>
2021-05-28 00:45:03 +1000

    mark methods public

--- src/angoka.ts
+++ src/angoka.ts
@@ -2,7 +2,7 @@ import { VevoAngo } from './algorithms/VevoAngo';
 
 export class Angoka {
     //This function returns a function that returns a number.
-    static VevoAngo(): (packInput: string) => number {
+    public static VevoAngo(): (packInput: string) => number {
         const func = (userInput: string): number => {
             let hashClass = new VevoAngo(userInput);
             let output = hashClass.compute();
@@ -12,7 +12,7 @@ export class Angoka {
         return func;
     }
 
-    static noHash(): (packInput: string) => string {
+    public static noHash(): (packInput: string) => string {
         const func = (userInput: string): string => {
             return userInput;
         }
